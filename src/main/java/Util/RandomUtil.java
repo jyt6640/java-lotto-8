9c20efcec6fbcec4a7357073c79ecc37f8f32c6f
jyt6640 <jyt6640@gmail.com>
2025-10-29 21:50:19 +0900

    feat(RandomUtil): 로또 번호 6개 뽑는 기능 구현

--- src/main/java/Util/RandomUtil.java
+++ src/main/java/Util/RandomUtil.java
@@ -4,7 +4,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class RandomUtil {
+    private static final int DEFAULT_COUNT = 6;
+
     public static List<Integer> getLottoNumber(int count) {
         return Randoms.pickUniqueNumbersInRange(1,45, count);
+    }
+
+    public static List<Integer> getLotto() {
+        return getLottoNumber(DEFAULT_COUNT);
     }
 }
