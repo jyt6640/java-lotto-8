d5b10637d57d916db64ca9c6351eb6360e15c203
jyt6640 <jyt6640@gmail.com>
2025-10-29 21:39:42 +0900

    test(RandomUtilTest): 로또 번호의 숫자가 1~45 범위 내에 있는지 확인 테스트 추가

---
+++ src/test/java/lotto/util/RandomUtilTest.java
@@ -0,0 +1,19 @@
+package lotto.util;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RandomUtilTest {
+    @DisplayName("로또 번호의 숫자가 1에서 45 범위 내에 있는지 확인")
+    @Test
+    void 로또_번호의_숫자가_1에서_45_범위_내에_있는지_확인() {
+        //given
+        List<String> input = RandomUtil.getLottoNumber(1);
+
+        //when&then
+        assertThat(input.getFirst()).isBetween(1, 45);
+    }
+}
