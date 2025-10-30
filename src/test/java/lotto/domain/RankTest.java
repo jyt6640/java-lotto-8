8451b94654bc3f6d818af56da59f0f62c56bb5e8
jyt6640 <jyt6640@gmail.com>
2025-10-30 17:21:05 +0900

    test(RankTest): 로또 번호가 5개 일치하고 보너스 번호가 있을 시 2등 테스트 추가

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -16,4 +16,15 @@ public class RankTest {
         assertThat(input).isEqualTo(Rank.FIRST);
         assertThat(input.getPrize()).isEqualTo(2_000_000_000L);
     }
+
+    @DisplayName("다섯개 일치하고 보너스 번호가 있으면 2등")
+    @Test
+    void 다섯개_일치하고_보너스_번호가_포함되어_있으면_2등() {
+        //given&when
+        Rank input = Rank.of(5, true);
+
+        //then
+        assertThat(input).isEqualTo(Rank.SECOND);
+        assertThat(input.getPrize()).isEqualTo(30_000_000L);
+    }
 }
