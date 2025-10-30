64c8c059d07cafade13762d1bca7ab6d19a7312d
jyt6640 <jyt6640@gmail.com>
2025-10-30 20:26:48 +0900

    test(LottosTest): 일급 컬렉션 Lottos 생성 테스트 추가

---
+++ src/test/java/lotto/domain/LottosTest.java
@@ -0,0 +1,44 @@
+package lotto.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottosTest {
+    @DisplayName("여러 로또 생성")
+    @Test
+    void 여러_로또_생성() {
+        //given
+        List<Lotto> input = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 5, 6, 7)),
+                new Lotto(List.of(15, 23, 25, 42, 37, 32)),
+                new Lotto(List.of(6, 32, 12, 34, 28, 16))
+        );
+
+        //when&then
+        assertThatCode(() -> new Lottos(input)).doesNotThrowAnyException();
+    }
+
+    @DisplayName("로또 개수 반환")
+    @Test
+    void 로또_개수_반환() {
+        //given
+        List<Lotto> input = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 5, 6, 7)),
+                new Lotto(List.of(15, 23, 25, 42, 37, 32)),
+                new Lotto(List.of(6, 32, 12, 34, 28, 16))
+        );
+
+        //when
+        Lottos result = new Lottos(input);
+
+        //then
+        assertThat(result.getLottos().size()).isEqualTo(4);
+    }
+
+}
