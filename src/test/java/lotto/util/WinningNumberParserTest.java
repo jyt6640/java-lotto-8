f66bf99c53afd2c48090736aa3fa2f3ee41a0952
jyt6640 <jyt6640@gmail.com>
2025-10-30 22:05:52 +0900

    test(WinningNumberParserTest): 당첨 번호 입력 후 컬렉션으로 반환 테스트 추가

--- src/test/java/lotto/util/WinningNumberParserTest.java
+++ src/test/java/lotto/util/WinningNumberParserTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class WinningNumberExtractorTest {
+public class WinningNumberParserTest {
     @DisplayName("보너스 번호 1개 분리")
     @Test
     void 보너스_번호_1개_분리() {
@@ -14,7 +14,7 @@ public class WinningNumberExtractorTest {
         List<Integer> input = List.of(1, 2, 3, 4, 5, 6, 7);
 
         //when
-        int bonusNumber = WinningNumberExtractor.extractBonusNumber(input);
+        int bonusNumber = WinningNumberParser.extractBonusNumber(input);
 
         //then
         assertThat(bonusNumber).isEqualTo(input.getLast());
@@ -27,10 +27,23 @@ public class WinningNumberExtractorTest {
         List<Integer> input = List.of(1, 2, 3, 4, 5, 6, 7);
 
         //when
-        List<Integer> winningNumbers = WinningNumberExtractor.extractWinningNumbers(input);
+        List<Integer> winningNumbers = WinningNumberParser.extractWinningNumbers(input);
 
         //then
         assertThat(winningNumbers.size()).isEqualTo(6);
         assertThat(winningNumbers).isEqualTo(input.subList(0,6));
+    }
+
+    @DisplayName("당첨 번호 입력 후 컬렉션으로 변환")
+    @Test
+    void 당첨_번호_입력_후_컬렉션으로_변환() {
+        //given
+        String input = "1,2,3,4,5,6";
+
+        //when
+        List<Integer> result = WinningNumberParser.parseWinningNumbers(input);
+
+        //then
+        assertThat(result).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
     }
 }
