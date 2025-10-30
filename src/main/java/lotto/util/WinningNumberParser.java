9554fffeec1ae0db1d14ae1129cea7d75a809901
jyt6640 <jyt6640@gmail.com>
2025-10-30 22:08:42 +0900

    feat(WinningNumberParser): 당첨 번호 입력 후 컬렉션으로 반환 기능 구현

--- src/main/java/lotto/util/WinningNumberParser.java
+++ src/main/java/lotto/util/WinningNumberParser.java
@@ -1,13 +1,21 @@
 package lotto.util;
 
+import java.util.Arrays;
 import java.util.List;
 
-public class WinningNumberExtractor {
+public class WinningNumberParser {
     public static int extractBonusNumber(List<Integer> getWinningNumbers) {
         return getWinningNumbers.getLast();
     }
 
     public static List<Integer> extractWinningNumbers(List<Integer> getWinningNumbers) {
         return getWinningNumbers.subList(0, 6);
+    }
+
+    public static List<Integer> parseWinningNumbers(String input) {
+        return Arrays.stream(input.split(","))
+                .map(String::strip)
+                .map(Integer::parseInt)
+                .toList();
     }
 }
