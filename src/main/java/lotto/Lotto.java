76a71d6d687deca1d66d4ec159be280caa3e26ca
jyt6640 <jyt6640@gmail.com>
2025-10-30 16:30:15 +0900

    feat(Lotto): 로또 번호 중복 및 개수 검증 기능 구현
    - validateDuplicateNumber로 중복 검증 기능 구현
    - validateNumberCount로 로또 번호 개수가 6인지 확인하는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,6 +13,18 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateNumberCount(numbers);
+        validateDuplicateNumber(numbers);
+    }
+
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        Set<Integer> validateSet = new HashSet<>(numbers);
+        if(validateSet.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+        }
+    }
+
+    private void validateNumberCount(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
