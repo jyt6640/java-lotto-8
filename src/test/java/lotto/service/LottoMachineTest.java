fbeca6783d3bbea412da1ae4d04b8dfff9d6bf66
jyt6640 <jyt6640@gmail.com>
2025-10-30 20:49:53 +0900

    test(LottoMachineTest): 구입 금액에 따라 로또를 발행하는 테스트 추가

---
+++ src/test/java/lotto/service/LottoMachineTest.java
@@ -0,0 +1,35 @@
+package lotto.service;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import lotto.domain.Lottos;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class LottoMachineTest {
+    private LottoMachine machine;
+
+    @BeforeEach
+    public void setUp() {
+        machine = new LottoMachine();
+    }
+
+    @DisplayName("구입 금액에 따라 로또를 발행")
+    @CsvSource({
+            "1000, 1",
+            "2000, 2",
+            "4000, 4",
+            "8000, 8",
+            "16000, 16"
+    })
+    @ParameterizedTest
+    void 구입_금액에_따라_로또를_발행(int inputAmount, int expectedCount) {
+        //given&when
+        Lottos lottos = machine.purchase(inputAmount);
+
+        //then
+        assertThat(lottos.getLottos().size()).isEqualTo(expectedCount);
+    }
+}
