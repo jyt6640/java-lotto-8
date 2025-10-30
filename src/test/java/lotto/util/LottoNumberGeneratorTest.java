cd37bbd5897deb3633691555ac79042a2ed2b121
jyt6640 <jyt6640@gmail.com>
2025-10-30 14:55:24 +0900

    test(util): 추출 테스트와 랜덤번호 생성 테스트 분리

--- src/test/java/lotto/util/LottoNumberGeneratorTest.java
+++ src/test/java/lotto/util/LottoNumberGeneratorTest.java
@@ -2,19 +2,19 @@ package lotto.util;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import Util.RandomUtil;
+import Util.LottoNumberGenerator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class RandomUtilTest {
+public class LottoNumberGeneratorTest {
     @DisplayName("로또 번호의 숫자가 1에서 45 범위 내에 있는지 확인")
     @Test
     void 로또_번호의_숫자가_1에서_45_범위_내에_있는지_확인() {
         //given
-        List<Integer> input = RandomUtil.getLottoNumber(1);
+        List<Integer> input = LottoNumberGenerator.getLottoNumber(1);
 
         //when&then
         assertThat(input.getFirst()).isBetween(1, 45);
@@ -24,7 +24,7 @@ public class RandomUtilTest {
     @Test
     void 로또_번호_6개_뽑기() {
         //given
-        List<Integer> input = RandomUtil.getLotto();
+        List<Integer> input = LottoNumberGenerator.getLotto();
 
         //when&then
         assertThat(input.size()).isEqualTo(6);
@@ -34,7 +34,7 @@ public class RandomUtilTest {
     @Test
     void 하나의_로또를_발행_할_때_중복되지_않는_숫자_발행() {
         //given
-        List<Integer> input = RandomUtil.getLotto();
+        List<Integer> input = LottoNumberGenerator.getLotto();
         Set<Integer> uniqueNumbers = new HashSet<>();
 
         //when
@@ -48,7 +48,7 @@ public class RandomUtilTest {
     @Test
     void 여섯_개의_당첨_번호_및_보너스_번호_뽑기() {
         //given
-        List<Integer> input = RandomUtil.getWinningNumbers(7);
+        List<Integer> input = LottoNumberGenerator.getWinningNumbers(7);
 
         //when&then
         assertThat(input.size()).isEqualTo(7);
@@ -58,7 +58,7 @@ public class RandomUtilTest {
     @Test
     void 중복되지_않는_당첨_번호_및_보너스_번호_발행() {
         //given
-        List<Integer> input = RandomUtil.getWinningLotto();
+        List<Integer> input = LottoNumberGenerator.getWinningLotto();
         Set<Integer> uniqueNumbers = new HashSet<>();
 
         //when
@@ -68,29 +68,5 @@ public class RandomUtilTest {
         assertThat(uniqueNumbers.size()).isEqualTo(7);
     }
 
-    @DisplayName("보너스 번호 1개 분리")
-    @Test
-    void 당첨_번호_6개와_보너스_번호_1개_분리() {
-        //given
-        List<Integer> input = RandomUtil.getWinningLotto();
 
-        //when
-        int bonusNumber = RandomUtil.extractBonusNumber(input);
-
-        //then
-        assertThat(bonusNumber).isEqualTo(input.getLast());
-    }
-
-    @DisplayName("당첨 번호 6개 분리")
-    @Test
-    void 당첨_번호_6개_분리() {
-        //given
-        List<Integer> input = RandomUtil.getWinningLotto();
-
-        //when
-        List<Integer> winningNumbers = RandomUtil.extractWinningNumbers(input);
-
-        //then
-        assertThat(winningNumbers).isEqualTo(input.subList(0,5));
-    }
 }
---
+++ src/test/java/lotto/util/WinningNumberExtractorTest.java
@@ -0,0 +1,37 @@
+package lotto.util;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import Util.WinningNumberExtractor;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WinningNumberExtractorTest {
+    @DisplayName("보너스 번호 1개 분리")
+    @Test
+    void 보너스_번호_1개_분리() {
+        //given
+        List<Integer> input = List.of(1, 2, 3, 4, 5, 6, 7);
+
+        //when
+        int bonusNumber = WinningNumberExtractor.extractBonusNumber(input);
+
+        //then
+        assertThat(bonusNumber).isEqualTo(input.getLast());
+    }
+
+    @DisplayName("당첨 번호 6개 분리")
+    @Test
+    void 당첨_번호_6개_분리() {
+        //given
+        List<Integer> input = List.of(1, 2, 3, 4, 5, 6, 7);
+
+        //when
+        List<Integer> winningNumbers = WinningNumberExtractor.extractWinningNumbers(input);
+
+        //then
+        assertThat(winningNumbers.size()).isEqualTo(6);
+        assertThat(winningNumbers).isEqualTo(input.subList(0,6));
+    }
+}
