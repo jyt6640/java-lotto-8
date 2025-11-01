46f1d930981996e8e4e395434ebc80ec316dcd17
jyt6640 <jyt6640@gmail.com>
2025-11-01 11:38:09 +0900

    feat(LottoResultService): 여러 장의 로또 당첨 통계 계산 기능 구현

--- src/main/java/lotto/service/LottoResultService.java
+++ src/main/java/lotto/service/LottoResultService.java
@@ -1,6 +1,9 @@
 package lotto.service;
 
+import java.util.EnumMap;
+import java.util.Map;
 import lotto.domain.Lotto;
+import lotto.domain.Lottos;
 import lotto.domain.Rank;
 import lotto.domain.WinningLotto;
 
@@ -10,5 +13,24 @@ public class LottoResultService {
         boolean hasBonusNumber = winningLotto.hasBonusNumber(lotto);
 
         return Rank.of(matchCount, hasBonusNumber);
+    }
+
+    public Map<Rank, Integer> getStatistics(WinningLotto winningLotto, Lottos lottos) {
+        Map<Rank, Integer> statistics = initializeStatistics();
+        for (Lotto lotto : lottos.getLottos()) {
+            Rank rank = getRank(winningLotto, lotto);
+            statistics.put(rank, statistics.get(rank) + 1);
+        }
+
+        return statistics;
+    }
+
+    private Map<Rank, Integer> initializeStatistics() {
+        Map<Rank, Integer> statistics = new EnumMap<>(Rank.class);
+        for (Rank rank : Rank.values()) {
+            statistics.put(rank, 0);
+        }
+
+        return statistics;
     }
 }
