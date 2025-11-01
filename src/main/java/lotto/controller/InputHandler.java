8834753d55a682a94f9d85f5146058289e86afc6
jyt6640 <jyt6640@gmail.com>
2025-11-01 20:37:42 +0900

    feat(InputHandler): 공백 입력 시 예외 발생 기능 구현

--- src/main/java/lotto/controller/InputHandler.java
+++ src/main/java/lotto/controller/InputHandler.java
@@ -13,17 +13,20 @@ public class InputHandler {
 
     public int readPurchaseAmount() {
         String price = inputview.readPurchaseAmount().strip();
+        validateWhiteSpace(price);
         validateOnlyNumber(price);
         return Integer.parseInt(price);
     }
 
     public List<Integer> readWinningNumbers() {
         String winningNumbers = inputview.readWinningLottoNumbers().strip();
+        validateWhiteSpace(winningNumbers);
         return WinningNumberParser.parseWinningNumbers(winningNumbers);
     }
 
     public int readBonusNumber() {
         String bonusNumber = inputview.readBonusNumber().strip();
+        validateWhiteSpace(bonusNumber);
         return WinningNumberParser.parseBonusNumber(bonusNumber);
     }
 
@@ -32,5 +35,11 @@ public class InputHandler {
             return;
         }
         throw new IllegalArgumentException("[ERROR] 구매 금액은 숫자만 입력 가능합니다.");
+    }
+
+    private void validateWhiteSpace(String input) {
+        if(input.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 입력값은 공백일 수 없습니다.");
+        }
     }
 }
