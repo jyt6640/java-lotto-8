ac01c660d06cedff01ac381752271536603858f4
jyt6640 <jyt6640@gmail.com>
2025-11-01 21:43:06 +0900

    feat(Money): 천원 단위로 입력하지 않을 시 예외 발생 기능 구현

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -14,11 +14,18 @@ public class Money {
 
     public void validate(int amount) {
         validateMinAmount(amount);
+        validateUnit(amount);
     }
 
     private void validateMinAmount(int amount) {
         if (amount < 1000) {
             throw new IllegalArgumentException("[ERROR] 최소 금액은 1,000원 이상이어야 합니다.");
+        }
+    }
+
+    private void validateUnit(int amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 금액은 1,000원 단위이어야 합니다.");
         }
     }
 }
