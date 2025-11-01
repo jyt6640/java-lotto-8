20fe6accedd5833cfe92e19658debb5aa90becb4
jyt6640 <jyt6640@gmail.com>
2025-11-01 21:39:42 +0900

    feat(Money): 천원 미만 입력 시 예외 발생 기능 구현

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -4,10 +4,21 @@ public class Money {
     private final int amount;
 
     public Money(int amount) {
+        validate(amount);
         this.amount = amount;
     }
 
     public int calculateLottoCount() {
         return amount / 1000;
+    }
+
+    public void validate(int amount) {
+        validateMinAmount(amount);
+    }
+
+    private void validateMinAmount(int amount) {
+        if (amount < 1000) {
+            throw new IllegalArgumentException("[ERROR] 최소 금액은 1,000원 이상이어야 합니다.");
+        }
     }
 }
