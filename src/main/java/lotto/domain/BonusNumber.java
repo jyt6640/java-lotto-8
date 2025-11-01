e117ae8d319252165d3bb7d2d5dfd804f76c7e86
jyt6640 <jyt6640@gmail.com>
2025-11-01 22:22:22 +0900

    feat(BonusNumber): 보너스 넘버가 1에서 45 범위를 넘어갈 시 예외 발생 기능 구현

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -4,6 +4,17 @@ public class BonusNumber {
     private final int bonusNumber;
 
     public BonusNumber(int bonusNumber) {
+        validate(bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(int bonusNumber) {
+        validateRange(bonusNumber);
+    }
+
+    private void validateRange(int bonusNumber) {
+        if (bonusNumber < 0 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 1~45 범위 값만 허용됩니다.");
+        }
     }
 }
