c2e1a54038e95037b0639ddec3583c104e409b51
jyt6640 <jyt6640@gmail.com>
2025-11-01 10:36:28 +0900

    test(WinningLottoTest): 테스트 로직 변경
    - 기존 당첨 번호 일치 개수 테스트 lotto 객체를 생성하여서 검증
    - 보너스 여부도 동일하게 lotto 객체를 생성하여 검증

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -15,7 +15,7 @@ public class WinningLottoTest {
     @MethodSource("provideMatchCases")
     @ParameterizedTest
     void 당첨_번호와_일치하는_개수_반환(
-            List<Integer> myNumbers,
+            Lotto myNumbers,
             List<Integer> winningNumber,
             int bonusNumber,
             int expectedCount
@@ -33,49 +33,49 @@ public class WinningLottoTest {
     private static Stream<Arguments> provideMatchCases() {
         return Stream.of(
                 Arguments.of(
-                        List.of(1, 2, 3, 4, 5, 6),
+                        new Lotto(List.of(1, 2, 3, 4, 5, 6)),
                         List.of(1, 2, 3, 4, 5, 6),
                         30,
                         6
                 ),
                 Arguments.of(
-                        List.of(1, 2, 3, 4, 5, 6),
+                        new Lotto(List.of(1, 2, 3, 4, 5, 6)),
                         List.of(1, 2, 3, 4, 5, 7),
                         30,
                         5
                 ),
                 Arguments.of(
-                        List.of(1, 2, 3, 4, 5, 6),
+                        new Lotto(List.of(1, 2, 3, 4, 5, 6)),
                         List.of(1, 2, 3, 4, 8, 7),
                         30,
                         4
                 ),
                 Arguments.of(
-                        List.of(1, 2, 3, 4, 5, 6),
+                        new Lotto(List.of(1, 2, 3, 4, 5, 6)),
                         List.of(1, 2, 3, 9, 8, 7),
                         30,
                         3
                 ),
                 Arguments.of(
-                        List.of(1, 2, 3, 4, 5, 6),
+                        new Lotto(List.of(1, 2, 3, 4, 5, 6)),
                         List.of(1, 2, 10, 9, 8, 7),
                         30,
                         2
                 ),
                 Arguments.of(
-                        List.of(1, 2, 3, 4, 5, 6),
+                        new Lotto(List.of(1, 2, 3, 4, 5, 6)),
                         List.of(1, 11, 10, 9, 8, 7),
                         30,
                         1
                 ),
                 Arguments.of(
-                        List.of(1, 2, 3, 4, 5, 6),
+                        new Lotto(List.of(1, 2, 3, 4, 5, 6)),
                         List.of(12, 11, 10, 9, 8, 7),
                         30,
                         0
                 ),
                 Arguments.of(
-                        List.of(1, 2, 3, 4, 5, 6),
+                        new Lotto(List.of(1, 2, 3, 4, 5, 6)),
                         List.of(6, 5, 4, 3, 2, 1),
                         30,
                         6
@@ -87,10 +87,11 @@ public class WinningLottoTest {
     @Test
     void 보너스_번호가_있는지_확인() {
         //given
-        WinningLotto myLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 6);
+        WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 6);
+        Lotto myLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
 
         //when
-        boolean result = myLotto.hasBonusNumber();
+        boolean result = winningLotto.hasBonusNumber(myLotto);
 
         assertThat(result).isTrue();
     }
