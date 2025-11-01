8e05e7ab4cbd1c14fd014aeb5be1eada72dd0732
jyt6640 <jyt6640@gmail.com>
2025-11-01 22:22:10 +0900

    test(BonusNumberTest): 보너스 넘버가 1에서 45 범위를 넘어갈 시 예외 발생 테스트 추가

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -1,9 +1,12 @@
 package lotto.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class BonusNumberTest {
     @DisplayName("BonusNumber 정상 생성")
@@ -15,5 +18,15 @@ public class BonusNumberTest {
         //when&then
         assertThatCode(() -> new BonusNumber(input))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("보너스 넘버가 1에서 45 범위를 넘어갈 시 예외 발생")
+    @ValueSource(ints = {0, 56, -1, -52})
+    @ParameterizedTest
+    void 보너스_넘버가_1에서_45_범위를_넘어갈_시_예외_발생 (int input) {
+        //when&then
+        assertThatThrownBy(() -> new BonusNumber(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 1~45 범위 값만 허용됩니다.");
     }
 }
