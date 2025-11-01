ba50af2503cc4e8c7a44319b70b9c2df9941b073
jyt6640 <jyt6640@gmail.com>
2025-11-01 12:35:23 +0900

    feat: 초기 기능만 구현 완료

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,18 +1,37 @@
 package lotto;
 
+import java.util.List;
+import java.util.Map;
 import lotto.domain.Lottos;
+import lotto.domain.Rank;
+import lotto.domain.WinningLotto;
 import lotto.service.LottoMachine;
+import lotto.service.LottoResultService;
+import lotto.util.WinningNumberParser;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         LottoMachine machine = new LottoMachine();
+        LottoResultService resultService = new LottoResultService();
 
-        String purchaseAmount = InputView.readPurchaseAmount();
-        int amount = Integer.parseInt(purchaseAmount);
-        Lottos Lottos = machine.purchase(amount);
+        String purchaseAmountStr = InputView.readPurchaseAmount();
+        int purchaseAmount = Integer.parseInt(purchaseAmountStr);
 
-        OutputView.printMyLottos(amount, Lottos);
+        Lottos myLottos = machine.purchase(purchaseAmount);
+        OutputView.printMyLottos(purchaseAmount, myLottos);
+
+        String winningNumbersStr = InputView.readWinningLottoNumbers();
+        List<Integer> winningNumbers = WinningNumberParser.parseWinningNumbers(winningNumbersStr);
+        String bonusNumberStr = InputView.readBonusNumber();
+        int bonusNumber = WinningNumberParser.parseBonusNumber(bonusNumberStr);
+
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+
+        Map<Rank, Integer> statistics = resultService.getStatistics(winningLotto, myLottos);
+        double profitRate = resultService.calculateProfitRate(statistics, purchaseAmount);
+
+        OutputView.printResult(statistics, profitRate);
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
+import java.util.Map;
 import lotto.domain.Lottos;
+import lotto.domain.Rank;
 
 public class OutputView {
     private OutputView() {
@@ -12,5 +14,15 @@ public class OutputView {
         lottos.getLottos().forEach(lotto ->
             System.out.println(lotto.getNumbers())
         );
+    }
+
+    public static void printResult(Map<Rank, Integer> statistics, double profitRate) {
+        System.out.println();
+        System.out.println("3개 일치 (5,000원) - " + statistics.get(Rank.FIFTH) + "개");
+        System.out.println("4개 일치 (50,000원) - " + statistics.get(Rank.FOURTH) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + statistics.get(Rank.THIRD) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + statistics.get(Rank.SECOND) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + statistics.get(Rank.FIRST) + "개");
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", profitRate);
     }
 }
