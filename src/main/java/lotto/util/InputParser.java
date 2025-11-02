7eea981ce1623b99a60429b4c4c0bfe074eda1c0
jyt6640 <jyt6640@gmail.com>
2025-11-02 10:42:46 +0900

    refactor(InputParser): 사용하지 않는 메서드 제거
    - extractWinningNumbers 기존 랜덤값에서 추출했던 로직이기에 제거
    - extractBonusNumber도 기존 랜덤값에서 추출했던 로직이기에 제거

--- src/main/java/lotto/util/InputParser.java
+++ src/main/java/lotto/util/InputParser.java
@@ -4,14 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InputParser {
-    public static int extractBonusNumber(List<Integer> getWinningNumbers) {
-        return getWinningNumbers.getLast();
-    }
-
-    public static List<Integer> extractWinningNumbers(List<Integer> getWinningNumbers) {
-        return getWinningNumbers.subList(0, 6);
-    }
-
     public static List<Integer> parseWinningNumbers(String input) {
         return Arrays.stream(input.split(","))
                 .map(String::strip)
--- src/test/java/lotto/util/InputParserTest.java
+++ src/test/java/lotto/util/InputParserTest.java
@@ -8,33 +8,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class InputParserTest {
-    @DisplayName("보너스 번호 1개 분리")
-    @Test
-    void 보너스_번호_1개_분리() {
-        //given
-        List<Integer> input = List.of(1, 2, 3, 4, 5, 6, 7);
-
-        //when
-        int bonusNumber = InputParser.extractBonusNumber(input);
-
-        //then
-        assertThat(bonusNumber).isEqualTo(input.getLast());
-    }
-
-    @DisplayName("당첨 번호 6개 분리")
-    @Test
-    void 당첨_번호_6개_분리() {
-        //given
-        List<Integer> input = List.of(1, 2, 3, 4, 5, 6, 7);
-
-        //when
-        List<Integer> winningNumbers = InputParser.extractWinningNumbers(input);
-
-        //then
-        assertThat(winningNumbers.size()).isEqualTo(6);
-        assertThat(winningNumbers).isEqualTo(input.subList(0,6));
-    }
-
     @DisplayName("당첨 번호 입력 후 컬렉션으로 변환")
     @Test
     void 당첨_번호_입력_후_컬렉션으로_변환() {
