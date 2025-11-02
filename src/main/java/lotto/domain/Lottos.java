25589592e95214e6249e8edfe79388f097d4dd7b
jyt6640 <jyt6640@gmail.com>
2025-11-02 11:29:49 +0900

    feat(Lottos): 로또 개수가 1 개 미만일 때 예외 발생 기능 구현

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -6,10 +6,17 @@ public class Lottos {
     private final List<Lotto> lottos;
 
     public Lottos(List<Lotto> lottos) {
+        validateEmptyLotto(lottos);
         this.lottos = List.copyOf(lottos);
     }
 
     public List<Lotto> getLottos() {
         return lottos;
+    }
+
+    public void validateEmptyLotto(List<Lotto> lottos) {
+        if (lottos.size() == 0) {
+            throw new IllegalArgumentException("[ERROR] 로또는 하나 이상 생성해야 합니다.");
+        }
     }
 }
