b3786c2e5b2104dc4c1aed5b40a4aa82c5abc39c
jyt6640 <jyt6640@gmail.com>
2026-01-01 22:06:05 +0900

    fix: Random으로 생성한 불변 리스트를 가변 리스트로 변환하여 테스트 통과하도록 수정

--- src/main/java/lotto/util/LottoNumberGenerator.java
+++ src/main/java/lotto/util/LottoNumberGenerator.java
@@ -7,8 +7,8 @@ import java.util.List;
 public class LottoNumberGenerator implements NumberGenerator {
     @Override
     public List<Integer> generate() {
-        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(1,45,6);
-        Collections.sort(lotto);
-        return lotto;
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6).stream()
+                .sorted()
+                .toList();
     }
 }
